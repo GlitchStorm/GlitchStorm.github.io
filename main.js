a7d87e9f177f587f1c20d27a220d5f3a685fa9ec
GlitchStorm <8481898+GlitchStorm@users.noreply.github.com>
2018-10-12 13:46:03 -0400

    YEET

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 var	money = 0;
+var clickUpgradeAmount = 0;
+var clickUpgradeCost = 0;
+var clickValue = 1;
 
-function moneyClick(clickValue) {
-	money = money + clickValue;
+function moneyClick() {
+	var clickValue = clickUpgradeAmount + 1;
+	money = money + (clickValue * 1);
 	document.getElementById('money').innerHTML = money;
-	}
+	};
 
 function upgradeClick(upgradeAmount){
-	
-}
+	var clickUpgradeCost = Math.floor(10 * Math.pow(1.1,clickUpgradeAmount));
+	if(money >= clickUpgradeCost){
+		clickUpgradeAmount = clickUpgradeAmount + 1;
+		money = money - clickUpgradeCost;
+		document.getElementById('clickUpgradeAmount').innerHTML = clickUpgradeAmount;
+		document.getElementById('money').innerHTML = money;
+	};
+	var nextClickUpgradeCost = Math.floor(10 * Math.pow(1.1,clickUpgradeAmount));
+	document.getElementById('clickUpgradeCost').innerHTML = nextClickUpgradeCost;
+};
